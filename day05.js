308e4e3080199b2cf09bbeab205393f2e2e32a1f
Mike Mathew <m2mathew@me.com>
2016-01-09 19:36:15 -0600

    Working on day 05

--- day05.js
+++ day05.js
@@ -5,3 +5,38 @@
  *
  */
 
+
+function findNaughtyOrNice (str) {
+
+	// var oneLine = str.split('\n');
+	// var vowels = ['a','e','i','o','u'];
+	// var doubles = ['aa','bb','cc','dd','ee','ff','gg','hh','ii','jj','kk','ll','mm','nn','oo','pp','qq','rr','ss','tt','uu','vv','ww','xx','yy','zz'];
+	// var notAllowed = ['ab','cd','pq','xy'];
+
+	// var nice = 0;
+	// var naughty = 0;
+
+	// oneLine.forEach(function() {
+
+
+	// });
+
+	var vowels = str.match( /[aeiou]{3}/g );
+	var doubles = str.match( /([a-z])\1/ );
+	var notAllowed = str.match( /ab|cd|pq|xy/ );
+
+	if ( (vowels != undefined && vowels.length > 2) && (doubles != undefined && doubles.length > 0) && (notAllowed == undefined || notAllowed.length > 0) ) {
+		console.log(str);
+	}
+	else {
+		console.log('nope');
+	}
+}
+
+// taken from advent-of-code website
+var input = 'aaa';
+
+
+
+findNaughtyOrNice(input);
+
